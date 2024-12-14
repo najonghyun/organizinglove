c747368ee95e141e923446d6cf2b780aca79b3b8
najonghyun <i0364842@naver.com>
2024-12-14 14:27:16 +0900

    feat: 추가 기능 개선 - 넘버링 변경, object 속도, about 변화

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React, { useRef, useState, useEffect } from "react";
-import BottomSheet from "./components/BottomSheet";
-import TopSheet from "./components/TopSheet";
-import "./App.css";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import useSound from "use-sound";
-import { ReactComponent as Question } from "./assets/question.svg";
-import { ReactComponent as OnHeart } from "./assets/onHeart.svg";
-import { ReactComponent as OffHeart } from "./assets/offHeart.svg";
-import { ReactComponent as ArrowRight } from "./assets/arrowRight.svg";
-import { ReactComponent as Speaker } from "./assets/speaker.svg";
-import { ReactComponent as BGMSpeaker } from "./assets/bgmSpeaker.svg";
-import { ReactComponent as Play } from "./assets/play.svg";
-import { ReactComponent as Pause } from "./assets/pause.svg";
-import { ReactComponent as ArrowDown } from "./assets/arrowDown.svg";
-import Loading from "./components/Loading";
+import React, { useRef, useState, useEffect } from 'react';
+import BottomSheet from './components/BottomSheet';
+import TopSheet from './components/TopSheet';
+import './App.css';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import useSound from 'use-sound';
+import { ReactComponent as Question } from './assets/question.svg';
+import { ReactComponent as OnHeart } from './assets/onHeart.svg';
+import { ReactComponent as OffHeart } from './assets/offHeart.svg';
+import { ReactComponent as ArrowRight } from './assets/arrowRight.svg';
+import { ReactComponent as Speaker } from './assets/speaker.svg';
+import { ReactComponent as BGMSpeaker } from './assets/bgmSpeaker.svg';
+import { ReactComponent as Play } from './assets/play.svg';
+import { ReactComponent as Pause } from './assets/pause.svg';
+import { ReactComponent as ArrowDown } from './assets/arrowDown.svg';
+import { ReactComponent as ArrowTop } from './assets/arrowTop.svg';
+import Loading from './components/Loading';
+
 const App = () => {
   const refreshPage = () => {
     window.location.reload();
@@ -26,7 +28,7 @@ const App = () => {
     Array.from({ length: circleCount }).map((_, i) => {
       const localIndex = i % 54;
       return {
-        id: i,
+        id: i + 1,
         state: false,
         left: Math.random() * (1366 * 2),
         top: Math.random() * (1024 * 2),
@@ -39,10 +41,8 @@ const App = () => {
   const circles = circlesRef.current;
   const [selected, setSelected] = useState([]);
   const handleCircleClick = (id) => {
-    if (showMessage) {
-      // 시작단계에서는 클릭 안되게
-      return;
-    }
+    if (showMessage) return;
+
     circles.forEach((circle) => {
       if (circle.id === id) {
         circle.state = !circle.state; // 클릭 상태 반전
@@ -66,7 +66,7 @@ const App = () => {
 
   const heartsRef = useRef(
     Array.from({ length: 10 }).map((_, i) => ({
-      id: i,
+      id: i + 1,
       state: false,
       left: Math.random() * (1366 * 2),
       top: Math.random() * (1024 * 2),
@@ -93,18 +93,29 @@ const App = () => {
 
   const [showMessage, setShowMessage] = useState(true); // 메시지 표시 여부
   const [isReady, setIsReady] = useState(false);
-  const [playBGM] = useSound("/sounds/bgm0.mp3", {
+  const [playBGM] = useSound('/sounds/bgm0.mp3', {
     loop: true,
+    html5: true,
     onload: () => {
-      console.log("오디오 준비 완료");
+      console.log('오디오 준비 완료');
       setIsReady(true);
     },
   });
+  // const playBGM = new Howl({
+  //   src: ['/sounds/bgm0.mp3'],
+  //   loop: true,
+  //   html5: true,
+  // });
+  // playBGM.once('load', () => {
+  //   console.log('오디오 준비 완료');
+  //   setIsReady(true);
+  // });
+
   const transformComponentRef = useRef(null);
   const handleInteraction = (e) => {
     e.preventDefault(); // 클릭 외 다른 이벤트 차단
     if (!isReady) {
-      console.log("아직 안됐다 임마!");
+      console.log('아직 안됐다 임마!');
       return;
     }
     setShowMessage(false); // 메시지 숨기기
@@ -112,7 +123,7 @@ const App = () => {
     if (transformComponentRef.current) {
       const delay = 300;
       setTimeout(() => {
-        transformComponentRef.current.zoomOut(0.5, 750, "easeOut");
+        transformComponentRef.current.zoomOut(0.5, 750, 'easeOut');
       }, delay);
     }
   };
@@ -140,7 +151,7 @@ const App = () => {
   const [topOpen, setTopOpen] = useState(false);
 
   const contentRef = useRef(null);
-  const handleCollect = () => {
+  const handleOrganized = () => {
     setCollect(!collect);
     if (contentRef.current) {
       contentRef.current.scrollTop = 0; // 스크롤 초기화
@@ -166,34 +177,34 @@ const App = () => {
     const resetTimer = () => {
       clearTimeout(timeoutId);
       timeoutId = setTimeout(() => {
-        console.log("Refreshing the page...");
+        console.log('Refreshing the page...');
         refreshPage();
-      }, 600000); // 10분 = 600,000ms
+      }, 300000); // 1분 = 60,000ms
     };
     resetTimer();
 
-    window.addEventListener("mousemove", resetTimer);
-    window.addEventListener("keydown", resetTimer);
+    window.addEventListener('mousemove', resetTimer);
+    window.addEventListener('keydown', resetTimer);
     return () => {
       clearTimeout(timeoutId);
-      window.removeEventListener("mousemove", resetTimer);
-      window.removeEventListener("keydown", resetTimer);
+      window.removeEventListener('mousemove', resetTimer);
+      window.removeEventListener('keydown', resetTimer);
     };
   }, []);
 
   return (
-    <div className="App" ref={scrollRef}>
+    <div className='App' ref={scrollRef}>
       <TopSheet open={topOpen}>
-        <div style={{ height: "100vh" }}>
-          <div className="topsheetText01">
+        <div style={{ height: '100vh' }}>
+          <div className='topsheetText01'>
             소리 정리하기 — 사랑으로
-            <div className="topsheetText02">
+            <div className='topsheetText02'>
               <br /> 엉킨 전선을 정리하듯 엉켜 떠다니는 ‘소리’도 정리할 수 있을까. <br /> 소리는
               언제나 세상에 부유하고 있고, 이 공간은 그러한 세상과도 같은 공간입니다.
-              <br /> 다만, 이 공간은 사랑에서 비롯된 소리로 이루어진 세상입니다. 사랑이라는 <br />{" "}
+              <br /> 다만, 이 공간은 사랑에서 비롯된 소리로 이루어진 세상입니다. 사랑이라는 <br />{' '}
               카테고리 아래 수집된 것들의 집합으로, 수많은 사랑의 메시지가 조각이 되어
               <br /> 부유하고 있습니다. 당신은 이 공간에서 부유하는 조각들을 찬찬히 듣고 보며
-              <br /> 사랑을 모을 수 있습니다. 당신이 생각하는 사랑에 가깝다면 멈추고 <br />{" "}
+              <br /> 사랑을 모을 수 있습니다. 당신이 생각하는 사랑에 가깝다면 멈추고 <br />{' '}
               선택하고, 수집해 주세요. 부유하던 조각들은 당신을 통해 나열되고 정리되어
               <br /> 당신만의 사랑의 소리로 생성됩니다. <br /> <br /> 이곳에서 당신이 생각하는
               사랑의 소리를 찾아 오랫동안 헤매어주세요.
@@ -203,18 +214,18 @@ const App = () => {
       </TopSheet>
       <BottomSheet open={bottomOpen} collect={collect}>
         <div>
-          <div className="bottomSheetPart01" />
-          <div className="bottomSheetPart02" />
+          <div className='bottomSheetPart01' />
+          <div className='bottomSheetPart02' />
         </div>
         <div
           ref={contentRef}
-          className="bottomSheetContent"
+          className='bottomSheetContent'
           style={{
-            top: collect ? "250px" : "0px",
-            height: "calc(100vh - 95px)",
-            overflowY: collect ? "scroll" : "hidden",
-            overflowX: "hidden",
-            transition: "top 0.6s ease",
+            top: collect ? '250px' : '0px',
+            height: 'calc(100vh - 95px)',
+            overflowY: collect ? 'scroll' : 'hidden',
+            overflowX: 'hidden',
+            transition: 'top 0.6s ease',
           }}
         >
           {selected.length > 0
@@ -223,76 +234,76 @@ const App = () => {
                 .reverse()
                 .map((s, index, array) => (
                   <div
-                    className="bottomsheetBox"
+                    className='bottomsheetBox'
                     key={index}
                     style={{
-                      height: !collect && index === 0 ? "210px" : "max-content",
+                      height: !collect && index === 0 ? '210px' : 'max-content',
                       paddingTop: collect
                         ? index === 0
-                          ? "40px"
-                          : "0px"
+                          ? '40px'
+                          : '0px'
                         : index !== 0
-                        ? "40px"
-                        : "0px",
+                        ? '40px'
+                        : '0px',
                       paddingBottom:
-                        collect && index === array.length - 1 ? "0px" : collect ? "24px" : "0px",
+                        collect && index === array.length - 1 ? '0px' : collect ? '24px' : '0px',
                     }}
                   >
-                    <div className="bottomsheetText">
+                    <div className='bottomsheetText'>
                       <IconWithSound selected={s} setHoverState={setHoverState} />
-                      <div className="bottomsheetNumber">{s.id}</div>
+                      <div className='bottomsheetNumber'>{s.id}</div>
                     </div>
-                    <img className="bottomsheetImage" src={s.image} alt={`Selected ${s.id}`} />
+                    <img className='bottomsheetImage' src={s.image} alt={`Selected ${s.id}`} />
                   </div>
                 ))
-            : ""}
+            : ''}
         </div>
       </BottomSheet>
       {/* 메시지 화면 */}
       {showMessage && (
-        <div className="startMessage">
-          <div className="startMessageBox">
-            <div className="startMessageText">
+        <div className='startMessage'>
+          <div className='startMessageBox'>
+            <div className='startMessageText'>
               이곳에서 당신이 생각하는 사랑의 소리를 찾아 오랫동안 헤매어주세요.
             </div>
-            <div className="startMessageIcon" onClick={handleInteraction}>
-              {isReady ? <ArrowRight width="16px" height="12px" /> : <Loading />}
+            <div className='startMessageIcon' onClick={handleInteraction}>
+              {isReady ? <ArrowRight width='16px' height='12px' /> : <Loading />}
             </div>
           </div>
         </div>
       )}
       <div
-        className="titleBox"
-        style={{ top: collect ? "-100vh" : "40px", transition: "top 0.5s ease" }}
+        className='titleBox'
+        style={{ top: collect ? '-100vh' : '40px', transition: 'top 0.5s ease' }}
       >
-        <div className="title" onClick={handleReset}>
+        <div className='title' onClick={handleReset}>
           Organizing the Sound — with Love
         </div>
-        <div className="subtitle">
+        <div className='subtitle'>
           Pause and collect sounds that you feel are the sounds of love.
         </div>
       </div>
 
       <div
-        className="organized"
-        onClick={handleCollect}
+        className='organized'
+        onClick={handleOrganized}
         style={{
-          bottom: bottomOpen && selected.length > 1 ? "40px" : "-340px",
-          transition: "bottom 0.5s ease",
+          bottom: bottomOpen && selected.length > 1 ? '40px' : '-50px',
+          transition: 'bottom 0.6s ease',
         }}
       >
-        <div className="organizedText">Organized</div>
-        <DelayedIcon collect={collect} selected={selected} />
+        <div className='organizedText'>Organized</div>
+        <OrganizedIcon collect={collect} selected={selected} />
       </div>
 
       <Plays collect={collect} selected={selected} />
-      <div className="about" onClick={handleAbout}>
+      <div className='about' onClick={handleAbout}>
         {hoverState.isHovered && hoverState.hasSound ? (
           <Speaker />
         ) : isBGMHovered ? (
           <BGMSpeaker />
         ) : (
-          <Question />
+          <AboutIcon about={topOpen} />
         )}
       </div>
       <TransformWrapper
@@ -318,28 +329,28 @@ const App = () => {
           disabled: false,
           size: 500,
           animationTime: 300,
-          animationType: "easeInOutCubic",
+          animationType: 'easeInOutCubic',
         }}
         velocityAnimation={{
           disabled: false,
           sensitivity: 0.1,
           animationTime: 300,
-          animationType: "easeOutQuint",
+          animationType: 'easeOutQuint',
         }}
       >
         {({ zoomOut, ...rest }) => (
           <TransformComponent>
-            <div className="container">
+            <div className='container'>
               {/* SVG 컨테이너 */}
               <svg
-                width="100%"
-                height="100%"
+                width='100%'
+                height='100%'
                 style={{
-                  position: "absolute",
+                  position: 'absolute',
                   top: 0,
                   left: 0,
                   zIndex: 1,
-                  pointerEvents: "none",
+                  pointerEvents: 'none',
                 }}
               >
                 {/* 선택된 원을 연결하는 선 */}
@@ -354,8 +365,8 @@ const App = () => {
                         y1={circle.top + 6}
                         x2={nextCircle.left + 6}
                         y2={nextCircle.top + 6}
-                        stroke="black"
-                        strokeWidth="1.3"
+                        stroke='black'
+                        strokeWidth='1.3'
                       />
                     );
                   })}
@@ -366,6 +377,7 @@ const App = () => {
                   circle={circle}
                   handleCircleClick={handleCircleClick}
                   setHoverState={setHoverState}
+                  showMessage={showMessage}
                 />
               ))}
               {hearts.map((heart) => (
@@ -374,6 +386,7 @@ const App = () => {
                   heart={heart}
                   handleHeartClick={handleHeartClick}
                   setIsBGMHovered={setIsBGMHovered}
+                  showMessage={showMessage}
                 />
               ))}
             </div>
@@ -385,11 +398,12 @@ const App = () => {
 };
 
 // Circle 컴포넌트
-const Circle = ({ circle, handleCircleClick, setHoverState }) => {
+const Circle = ({ circle, handleCircleClick, setHoverState, showMessage }) => {
   const [play, { stop }] = useSound(circle.sound, { interrupt: true, loop: true });
   const [hover, setHover] = useState(false);
 
   const handleMouseEnter = () => {
+    if (showMessage) return;
     if (circle.sound) {
       play(); //소리 재생
     } else {
@@ -402,6 +416,7 @@ const Circle = ({ circle, handleCircleClick, setHoverState }) => {
   };
 
   const handleMouseLeave = () => {
+    if (showMessage) return;
     if (circle.sound) {
       stop(); // 소리 정지
     }
@@ -414,26 +429,26 @@ const Circle = ({ circle, handleCircleClick, setHoverState }) => {
 
   return (
     <div
-      className="circleBox"
+      className='circleBox'
       style={{
-        position: "absolute",
+        position: 'absolute',
         left: `${circle.left}px`,
         top: `${circle.top}px`,
       }}
     >
       <div
-        className="circle"
+        className='circle'
         onClick={() => {
           handleCircleClick(circle.id);
         }} // 클릭 시 상태 변경
         onMouseEnter={handleMouseEnter} // 마우스 올릴 때 소리 재생
         onMouseLeave={handleMouseLeave} // 마우스 나갈 때 소리 정지
       >
-        <div className="circle2" style={{ backgroundColor: circle.state ? "black" : "#e9e9e9" }} />
+        <div className='circle2' style={{ backgroundColor: circle.state ? 'black' : '#e9e9e9' }} />
       </div>
       {!circle.sound && hover ? (
-        <object className="circleHoverImage" data={circle.image}>
-          {" "}
+        <object className='circleHoverImage' data={circle.image}>
+          {' '}
         </object>
       ) : null}
     </div>
@@ -441,15 +456,17 @@ const Circle = ({ circle, handleCircleClick, setHoverState }) => {
 };
 
 // Heart 컴포넌트
-const Heart = ({ heart, handleHeartClick, setIsBGMHovered }) => {
+const Heart = ({ heart, handleHeartClick, setIsBGMHovered, showMessage }) => {
   const [play, { stop }] = useSound(heart.sound, { interrupt: true, loop: true });
 
   const handleMouseEnter = () => {
+    if (showMessage) return;
     play();
     setIsBGMHovered(true);
   };
 
   const handleMouseLeave = () => {
+    if (showMessage) return;
     if (!heart.state) {
       stop();
     }
@@ -458,9 +475,9 @@ const Heart = ({ heart, handleHeartClick, setIsBGMHovered }) => {
 
   return (
     <div
-      className="heart"
+      className='heart'
       style={{
-        position: "absolute",
+        position: 'absolute',
         left: `${heart.left}px`,
         top: `${heart.top}px`,
       }}
@@ -470,7 +487,7 @@ const Heart = ({ heart, handleHeartClick, setIsBGMHovered }) => {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      {heart.state ? <OnHeart className="heart2" /> : <OffHeart className="heart2" />}
+      {heart.state ? <OnHeart className='heart2' /> : <OffHeart className='heart2' />}
     </div>
   );
 };
@@ -500,7 +517,7 @@ const IconWithSound = ({ selected, setHoverState }) => {
 
   return (
     <div
-      className="bottomsheetIcon"
+      className='bottomsheetIcon'
       onMouseEnter={handleMouseEnter} // 마우스 올릴 때 재생
       onMouseLeave={handleMouseLeave} // 마우스 나갈 때 정지
     />
@@ -569,7 +586,7 @@ const Plays = ({ collect, selected }) => {
   };
 
   const handleStop = () => {
-    console.log("중지 실행");
+    console.log('중지 실행');
     setPlaying(false);
     setCurrentIndex(-1);
     if (currentIndex !== -1 && stopRefs.current[currentIndex]) {
@@ -590,21 +607,21 @@ const Plays = ({ collect, selected }) => {
         />
       ))}
       <div
-        className="play"
+        className='play'
         style={{
-          bottom: collect ? "40px" : "-340px",
-          transition: "bottom 0.5s ease",
+          bottom: collect ? '40px' : '-340px',
+          transition: 'bottom 0.5s ease',
         }}
         onClick={playing ? handleStop : handlePlay}
       >
         {playing ? <Pause /> : <Play />}
-        <div className="playText">{playing ? "stop" : "play"}</div>
+        <div className='playText'>{playing ? 'stop' : 'play'}</div>
       </div>
     </>
   );
 };
 
-const DelayedIcon = ({ collect, selected }) => {
+const OrganizedIcon = ({ collect, selected }) => {
   const [delayed, setDelayed] = useState(null);
   const selectedRef = useRef(selected);
 
@@ -620,6 +637,20 @@ const DelayedIcon = ({ collect, selected }) => {
     selectedRef.current = selected;
     setDelayed(selected.length);
   }, [selected]);
+
+  return <div>{delayed}</div>;
+};
+
+const AboutIcon = ({ about }) => {
+  const [delayed, setDelayed] = useState(about ? <ArrowTop /> : <Question />);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDelayed(about ? <ArrowTop /> : <Question />);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [about]);
 
   return <div>{delayed}</div>;
 };
